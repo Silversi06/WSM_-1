4e62ea5a7b52849b0ae58f1179f4ad986b1563d6
GoEun <w2219@e-mirim.hs.kr>
2023-09-12 15:56:20 +0900

    now, 제목 달 보여주자, 1일 무슨 요일인지 가져오자, 마지막 날짜 가져오자, 실제 달력 표시하자(createElement,appendChild)

---
+++ 20230601_bookreviewcalender/js/calender.js
@@ -0,0 +1,57 @@
+//이번달
+//현재날짜, 시각 객체 만들자
+var now = new Date();
+//현재 년
+var year = now.getFullYear();
+
+//현재 월
+var month=now.getMonth()+1;
+//현재 일
+var date=now.getDate();
+
+const datesContainerDiv = document.querySelectorAll(".dates.container")[0];
+const setCalender = (year, month) => {
+    
+    //제목 달 보여주자
+    //html, -> js
+    const titleMonthDiv = document.getElementsByClassName("month")[0];
+    //.innerHTML =`${month}월`;
+    titleMonthDiv.innerHTML = `${month}월`;
+
+    //달력 만들자
+    //해당 월의 1일이 무슨 요일인지알아야한다
+    // 해당 월의 첫 번째 날짜를 가져옵니다.
+    var firstDate = new Date(year, month - 1, 1).getDay();
+    // 해당 월의 첫 번째 날짜의 요일을 가져옵니다. (0: 일요일, 1: 월요일, ..., 6: 토요일)
+
+    //해당 월의 마지막 날짜가 며칠? :다음 달의 1일 하루 전 날(0일)
+    var lastDate = new Date(year, month-1 +1, 0).getDate();
+
+    //.date.item{$}*lastDate
+    //for 1~lastday
+    
+    for(let date =1; date<=lastDate; date++){
+        //<div class = "date item"> date</div>
+        let dateItemDiv = document.createElement("div");
+        dateItemDiv.classList.add("date");  //<div class = "date"></div>
+        dateItemDiv.classList.add("item");//<div class = "date item"></div>
+        dateItemDiv.innerHTML = date;//<div class = "date">날짜</div>
+        //HTML 에 .dates.container 자식으로 넣자
+        datesContainerDiv.appendChild(dateItemDiv);
+    }
+
+    //1일을 firstDateDay로 옮기자 5->6
+    //.dates.container의 자식 중 첫째자식(1일) style grid-column-start: day+1
+    let firstDateDiv = datesContainerDiv.firstElementChild;
+    //CSS {grid-column-start: firstDateDay +1;}
+    firstDateDiv.style.gridColumnStart = firstDate +1;
+
+}
+
+setCalender(year, month);
+
+
+
+
+//<:이전 달
+//>:이후 달
